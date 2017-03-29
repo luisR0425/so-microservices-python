d1c5b6fee759312a9ed859530da386fb063095ba
luisR0425 <luisf0425@gmail.com>
2017-03-29 09:01:18 -0500

    Add files via upload

---
+++ A00315320/05_flask_app/users.py
@@ -0,0 +1,47 @@
+from flask import Flask, abort, request
+import json
+
+from users_commands import get_all_users, add_user, remove_user
+
+app = Flask(__name__)
+api_url = '/v1.0'
+
+@app.route(api_url+'/users',methods=['POST'])
+def create_user():
+  content = request.get_json(silent=True)
+  username = content['username']
+  password = content['password']
+  if not username or not password:
+    return "empty username or password", 400
+  if username in get_all_users():
+    return "user already exist", 400
+  if add_user(username,password):
+    return "user created", 201
+  else:
+    return "error while creating user", 400
+
+@app.route(api_url+'/users',methods=['GET'])
+def read_user():
+  list = {}
+  list["users"] = get_all_users()
+  return json.dumps(list), 200
+
+@app.route(api_url+'/users',methods=['PUT'])
+def update_user():
+  return "not found", 404
+
+@app.route(api_url+'/users',methods=['DELETE'])
+def delete_user():
+  error = False
+  for username in get_all_users():
+    if not remove_user(username):
+        error = True
+
+  if error:
+    return 'some users were not deleted', 400
+  else:
+    return 'all users were deleted', 200
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0',port=8080,debug='True')
+
